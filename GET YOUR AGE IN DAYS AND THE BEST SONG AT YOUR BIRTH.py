07e130012dad5da85eafd3b7a5cd1d776bd7cdc1
Muhammad Shahzain Gill <39150430+shahgill@users.noreply.github.com>
2018-08-08 23:31:22 +0400

    Add files via upload

---
+++ GET YOUR AGE IN DAYS AND THE BEST SONG AT YOUR BIRTH.py
@@ -0,0 +1,368 @@
+
+while True:
+
+
+     years = input("How many years?")
+     months = input("How many months?")
+     days = input("How many days?")
+     
+
+
+     if int(years) == 5:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2012 was GANGNAM STYLE by P.S.Y")
+
+                                             
+     elif int(years) == 6:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2011 was ROLLING IN THE DEEP by Adele")
+
+
+     elif int(years) == 7:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2010 was JUST THE WAY YOU ARE by Bruno Mars")    
+
+     elif int(years) == 8:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2009 was LOVE LOCKDOWN by Kanye West")
+
+
+
+     elif int(years) == 9:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+          
+          print("the most famous song in 2008 was LET IT ROCK by Kevin Rudolf")
+           
+
+     elif int(years) == 10:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+                      
+          print("the most famous song in 2007 was DONT STOP THE MUSIC by Rihanna")
+
+     elif int(years) == 11:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2006 was CRAZY by Gnarls Barkley")
+
+     elif int(years) == 12:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2005 was CNADY SHOP by 50 Cent")
+
+     elif int(years) == 13:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2004 was LET IT ROCK by Kevin Rudolf")
+          
+
+
+     elif int(years) == 14:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2003 was WHERE IS THE LOVE by Black Eyed Peas")
+
+
+     elif int(years) == 15:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2002 was LOSE YOURSELF by Eminem")
+
+
+     elif int(years) == 16:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 2001 was HANGING BY A MOMENT by Lifehouse")
+
+
+
+     elif int(years) == 17:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+                                 
+          print("the most famous song in 2000 was THE REAL SLIM SHADY by Eminem")
+
+
+     elif int(years) == 18:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1999 was WILD WILD WEST by Will Smith")
+
+     elif int(years) == 19:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1998 was DEJA VU by Peter Gunz")
+
+     elif int(years) == 20:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1997 was QUIT PLAYING GAMES by Backstreet Boys")
+
+     elif int(years) == 21:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1996 was UNTIL IT SLEEPS by Metallica")
+
+
+     elif int(years) == 22:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1995 was HOLD ME, THRILL ME, KISS ME by U2")
+
+          
+     elif int(years) == 23:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1994 was I'LL REMEBER by Madonna")
+
+     elif int(years) == 24:
+          print("You are",int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1993 was THAT'S THE WAY LOVE GOES by Jannet Jackson")
+
+     elif int(years) == 25:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1992 was LETS GET ROCKED by Def Leppard")
+
+     elif int(years) == 26:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1991 was BLACK OR WHITE by Micheal Jackson")
+
+     elif int(years) == 27:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+                                 
+          print("the most famous song in 1990 was ICE ICE BABY by Vanilla Ice")
+
+
+     elif int(years) == 28:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1989 was WE DIDN'T START THE FIRE by Billy Joel")
+
+     elif int(years) == 29:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1988 was SIMPLY IRRESISTIBLE by Robert Palmer")
+
+     elif int(years) == 30:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1987 was BAD by Micheal Jackson")
+
+     elif int(years) == 31:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1986 was SLEDGEHAMMER by Peter Gabriel")
+
+
+     elif int(years) == 32:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1985 was MONEY FOR NOTHING by Dire Straits")
+
+          
+     elif int(years) == 33:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1984 was WHEN DOVES CRY by Prince")
+
+     elif int(years) == 34:
+          print("You are",int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1984 was UPTOWN GIRL by Billy Joel")
+
+
+     elif int(years) == 35:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1982 was LOSE YOURSELF by Eminem")
+
+     elif int(years) == 36:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1981 was HANGING BY A MOMENT by Lifehouse")
+
+     elif int(years) == 37:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+                                 
+          print("the most famous song in 1980 was THE REAL SLIM SHADY by Eminem")
+
+
+     elif int(years) == 38:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1979 was WILD WILD WEST by Will Smith")
+
+     elif int(years) == 39:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1978 was DEJA VU by Peter Gunz")
+      
+     elif int(years) == 40:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1977 was QUIT PLAYING GAMES by Backstreet Boys")
+
+     elif int(years) == 41:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1976 was UNTIL IT SLEEPS by Metallica")
+
+
+     elif int(years) == 42:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1975 was HOLD ME, THRILL ME, KISS ME by U2")
+
+          
+     elif int(years) == 43:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1974 was I'LL REMEBER by Madonna")
+
+     elif int(years) == 44:
+          print("You are",int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1973 was THAT'S THE WAY LOVE GOES by Jannet Jackson")
+
+     elif int(years) == 45:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1972 was LOSE YOURSELF by Eminem")
+
+     elif int(years) == 46:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1971 was HANGING BY A MOMENT by Lifehouse")
+
+     elif int(years) == 47:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+                                 
+          print("the most famous song in 1970 was THE REAL SLIM SHADY by Eminem")
+
+
+     elif int(years) == 48:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1969 was WILD WILD WEST by Will Smith")
+
+     elif int(years) == 49:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1968 was DEJA VU by Peter Gunz")
+      
+     elif int(years) == 50:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1967 was QUIT PLAYING GAMES by Backstreet Boys")
+
+     elif int(years) == 51:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1966 was UNTIL IT SLEEPS by Metallica")
+
+
+     elif int(years) == 52:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1965 was HOLD ME, THRILL ME, KISS ME by U2")
+
+          
+     elif int(years) == 53:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1964 was I'LL REMEBER by Madonna")
+
+     elif int(years) == 54:
+          print("You are",int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1963 was THAT'S THE WAY LOVE GOES by Jannet Jackson")
+
+     elif int(years) == 55:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1962 was LOSE YOURSELF by Eminem")
+
+     elif int(years) == 56:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1961 was HANGING BY A MOMENT by Lifehouse")
+
+     elif int(years) == 57:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+                                 
+          print("the most famous song in 1960 was THE REAL SLIM SHADY by Eminem")
+
+           
+     elif int(years) == 58:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1959 was QUIT PLAYING GAMES by Backstreet Boys")
+
+     elif int(years) == 59:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1958 was UNTIL IT SLEEPS by Metallica")
+
+
+     elif int(years) == 60:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1957 was HOLD ME, THRILL ME, KISS ME by U2")
+
+          
+     elif int(years) == 61:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1956 was I'LL REMEBER by Madonna")
+
+     elif int(years) == 62:
+          print("You are",int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1955 was THAT'S THE WAY LOVE GOES by Jannet Jackson")
+
+     elif int(years) == 63:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1954 was LOSE YOURSELF by Eminem")
+
+     elif int(years) == 64:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1953 was HANGING BY A MOMENT by Lifehouse")
+
+     elif int(years) == 65:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+                                 
+          print("the most famous song in 1952 was THE REAL SLIM SHADY by Eminem")
+
+     elif int(years) == 66:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1951 was WILD WILD WEST by Will Smith")
+
+     elif int(years) == 67:
+          print("You are" ,int(years)*int(365)+int(months)*int(30)+int(days), "days old")
+
+          print("the most famous song in 1950 was DEJA VU by Peter Gunz")
+
+     else:
+          print("Age not supported")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
